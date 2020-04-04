01a6db3d9886a36a304af9171ae1fcf6b65bf2ab
Saumay-Agrawal <saumay@Saumays-MacBook-Pro.local>
2020-04-04 14:41:53 +0530

    Added API endpoints

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -22,6 +23,27 @@ type Orchestrator struct {
 	Active            bool   `json:"Active"`
 	Status            string `json:"Status"`
 	PricePerPixel     string `json:"PricePerPixel"`
+}
+
+type OrchestratorStats struct {
+	Address           string `json:"Address"`
+	ServiceURI        string `json:"ServiceURI"`
+	LastRewardRound   int    `json:"LastRewardRound"`
+	RewardCut         int    `json:"RewardCut"`
+	FeeShare          int    `json:"FeeShare"`
+	DelegatedStake    int64  `json:"DelegatedStake"`
+	ActivationRound   int    `json:"ActivationRound"`
+	DeactivationRound int64  `json:"DeactivationRound"`
+	Active            bool   `json:"Active"`
+	Status            string `json:"Status"`
+	PricePerPixel     string `json:"PricePerPixel"`
+	UpdatedAt         int    `json:"UpdatedAt"`
+}
+
+type PriceHistory struct {
+	Address       string `json:"Address"`
+	PricePerPixel string `json:"PricePerPixel"`
+	Time          int    `json:"Time"`
 }
 
 func initializeDB() {
@@ -75,10 +97,44 @@ func fetchAndStore() {
 	}
 }
 
+// Method to get data from Orchestrator Table
+func GetOrchestratorStats(w http.ResponseWriter, req *http.Request) {
+	database, _ := sql.Open("sqlite3", "./pricing_tool.db")
+	rows, _ := database.Query("SELECT * FROM Orchestrators")
+	orchestrators := []OrchestratorStats{}
+	x := OrchestratorStats{}
+	for rows.Next() {
+		rows.Scan(&x.Address, &x.ServiceURI, &x.LastRewardRound, &x.RewardCut, &x.FeeShare, &x.DelegatedStake, &x.ActivationRound, &x.DeactivationRound, &x.Active, &x.Status, &x.PricePerPixel, &x.UpdatedAt)
+		orchestrators = append(orchestrators, x)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(orchestrators)
+}
+
+// Method to get data from PriceHistory Table based on orchestrator ID
+func GetOrchestratorPriceHistory(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	database, _ := sql.Open("sqlite3", "./pricing_tool.db")
+	rows, _ := database.Query("SELECT * FROM PriceHistory WHERE Address=? ORDER BY Time DESC", params["address"])
+	data := []PriceHistory{}
+	x := PriceHistory{}
+	for rows.Next() {
+		rows.Scan(&x.Address, &x.Time, &x.PricePerPixel)
+		data = append(data, x)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
+
 func main() {
 
 	initializeDB()
 
 	fetchAndStore()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/orchestratorStats", GetOrchestratorStats).Methods("GET")
+	router.HandleFunc("/priceHistory/{address}", GetOrchestratorPriceHistory).Methods("GET")
+	http.ListenAndServe(":12345", router)
 
 }
